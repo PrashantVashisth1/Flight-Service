8a802633cf052445f6cf6730a589d001f11bed58
PrashantVashisth <prashantvashisth2004@gmail.com>
2025-06-29 16:21:17 +0530

    add flights with custom filters

--- src/routes/v1/flight-routes.js
+++ src/routes/v1/flight-routes.js
@@ -1,14 +1,19 @@
-const express = require('express');
+const express = require("express");
 
-const { FlightController } = require('../../controllers');
-const { FlightMiddleware } = require('../../middlewares');
+const { FlightController } = require("../../controllers");
+const { FlightMiddleware } = require("../../middlewares");
 
 const router = express.Router();
 
 // /api/v1/flights POST
-router.post('/', 
-        FlightMiddleware.validateCreateRequest,
-        FlightMiddleware.validateTimeRequest,
-        FlightController.createFlight);
+router.post(
+  "/",
+  FlightMiddleware.validateCreateRequest,
+  FlightMiddleware.validateTimeRequest,
+  FlightController.createFlight
+);
+
+// /api/v1/flights?trips=MUM-DEL
+router.get('/', FlightController.getAllFlights);
 
 module.exports = router;
