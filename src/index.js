4942f01ebdfd39aa4a1d0745cfffe35fc1936d94
PrashantVashisth <prashantvashisth2004@gmail.com>
2025-06-29 10:43:48 +0530

    Added flights model with foreign key constraint :

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const apiRoutes = require('./routes');
 const { ServerConfig } = require('./config');
+const {City} = require('./models')
 
 const app = express();
 
@@ -9,6 +10,6 @@ app.use(express.urlencoded({extended: true}));
 
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
   console.log(`Server is running on port ${ServerConfig.PORT}`);
 })
